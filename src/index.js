7fceb1e16b6a7124ac888604693c51c1c41ab8bb
YuliaGrinenko <yuliaazarova1@gmail.com>
2024-02-22 17:07:11 +0000

    Displaying city of search

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-//function searchApi(city) {
-//let apiKey = "195c2c787bc01a377e2ef01266be08ce";
-//let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=195c2c787bc01a377e2ef01266be08ce`;
-///axios.get(apiUrl).then(showWeather);
+function showWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let temperature = response.data.temperature;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city.name;
+  console.log(response);
+}
 
+function searchApi(city) {
+  let apiKey = "195c2c787bc01a377e2ef01266be08ce";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=195c2c787bc01a377e2ef01266be08ce&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
 function searchCity(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input").value;
   let city = document.querySelector("#current-city");
   city.innerHTML = searchInputElement;
+  searchApi(searchInputElement);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchCity);
