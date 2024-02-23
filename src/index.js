32e0e6a9380935283d14839ba5ed8cbf4b293240
YuliaGrinenko <yuliaazarova1@gmail.com>
2024-02-23 16:02:30 +0000

    weather parameters added, current time

--- src/index.js
+++ src/index.js
@@ -4,6 +4,38 @@ function showWeather(response) {
   temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  console.log(response.data);
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img src="response.data.weather[0].icon"/>`;
+  let timeElement = document.querySelector("#current-day-time");
+  let date = new Date(response.data.dt * 1000);
+  timeElement.innerHTML = formatDate(date);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  console.log();
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchApi(city) {
@@ -20,3 +52,5 @@ function searchCity(event) {
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchCity);
+
+searchApi("London");
